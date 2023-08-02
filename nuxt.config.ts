b16190499afd173e4f467a37ef4972feb7d7ca37
Jamile Merlin <jamilemerlin@gmail.com>
2023-08-02 23:29:48 +0200

    fix: add app.vue back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxt/content', '@nuxthq/ui'],
   content: {},
-  experimental: {
-    payloadExtraction: false,
-  }
 })
