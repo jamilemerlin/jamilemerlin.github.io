5f98114c383087b97290d19cd4473b2ddcc12999
Jamile Merlin <jamilemerlin@gmail.com>
2023-08-04 23:50:55 +0200

    (wip) create initial NOW

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jamilemerlin.github.io'
+  site: 'https://jamilemerlin.github.io',
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), mdx()]
 });
